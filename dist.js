7e18101247d41cc9c979c96244b842b396018f7f
sheodox <3468630+sheodox@users.noreply.github.com>
2020-02-29 20:53:47 -0600

    auto-increment the version number when building a release version, rename 'build' script to 'release'

--- dist.js
+++ dist.js
@@ -1,10 +1,23 @@
 const fs = require('fs'),
+	packagedScriptPath = `./dist/vrv-subtitler.user.js`,
+	lastPackagedScript = fs.readFileSync(packagedScriptPath).toString(),
 	script = fs.readFileSync(`./dist/main.js`).toString();
+
+let versionBase = '0.1.';
+const [_, oldVersion] = lastPackagedScript.match(/@version\s*\d+.\d+.(\d+)/);
+
+//if packaging a release version, increment the version
+if (process.argv.includes('release')) {
+	versionBase += parseInt(oldVersion) + 1;
+}
+else {
+	versionBase += oldVersion;
+}
 
 const userscript = `// ==UserScript==
 // @name         VRV Subtitler
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      ${versionBase}
 // @description  Display SRT format subtitles on VRV
 // @author       sheodox
 // @match        https://static.vrv.co/vilos/player.html
@@ -15,5 +28,5 @@ const userscript = `// ==UserScript==
 ${script}
 `;
 
-fs.writeFileSync(`./dist/vrv-subtitler.user.js`, userscript);
+fs.writeFileSync(packagedScriptPath, userscript);
 console.log(`script packaged`);
