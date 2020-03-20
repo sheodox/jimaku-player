e13a5039f8d96f13a6cd792e5b8eff109359f5d2
sheodox <3468630+sheodox@users.noreply.github.com>
2020-03-19 22:12:56 -0500

    use the history path specified by video-info as the video title

--- src/local-player/player/Player.svelte
+++ src/local-player/player/Player.svelte
@@ -1,5 +1,11 @@
-<span class="video-title">Test Video</span>
+<span class="video-title">{videoTitle}</span>
 <Video src={videoSrc} />
+{#if videoTitle}
+	<script src="/subtitler.js"></script>
+	<script>
+		console.log("Subtitler App", document.querySelector('.subtitles-app'));
+	</script>
+{/if}
 
 <style>
 	.video-title {
@@ -12,16 +18,23 @@
 	import viewTimes from '../view-times';
 	import Video from './Video.svelte';
 
-
+	let videoTitle = '';
 	let videoSrc = decodeURIComponent(location.search.replace(/^\?/, ''));
 
-	onMount(() => {
+	onMount(async () => {
 		const video = document.querySelector('video');
+		if (videoSrc) {
+			const videoInfo = await fetch(`/video-info?path=${encodeURIComponent(videoSrc)}`).then(res => res.json());
 
-		video.currentTime = viewTimes.get(videoSrc);
-		setInterval(() => {
-			viewTimes.set(videoSrc, video.currentTime)
-		}, 50);
+			videoTitle = videoInfo.history
+					.map(h => h.name)
+					.join('/');
+
+			video.currentTime = viewTimes.get(videoSrc);
+			setInterval(() => {
+				viewTimes.set(videoSrc, video.currentTime)
+			}, 50);
+		}
 
 	});
 </script>
