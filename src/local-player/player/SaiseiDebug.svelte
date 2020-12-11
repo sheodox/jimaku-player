afeae00df34ff5859f7bb7664b96f43b043ad4e9
sheodox <3468630+sheodox@users.noreply.github.com>
2020-12-11 10:09:02 -0600

    Added a way to download the stream probe metadata in the debug menu, used some more relevant icons for the download buttons, and made the whole modal a bit smaller as it'll probably hold more stuff in the future.

--- src/local-player/player/SaiseiDebug.svelte
+++ src/local-player/player/SaiseiDebug.svelte
@@ -4,19 +4,35 @@
     }
     h2 {
         font-size: 1.1rem;
+        margin: 0;
+    }
+    button {
+        font-size: 0.8rem;
     }
 </style>
 
 <div class="modal-body">
     <h2>Download MPD files</h2>
 
-    <button on:click={() => downloadMpd(metadata.name, metadata.mpd)}>
-        <Icon icon="download" />
+    <button on:click={() => downloadMpd(metadata.title, metadata.mpd)}>
+        <Icon icon="file-video" />
         Video
     </button>
     {#each metadata.audio as audio}
         <button on:click={() => downloadMpd(getTrackTitle(audio), audio.mpd)}>
-            <Icon icon="download" />
+            <Icon icon="file-audio" />
+            {getTrackTitle(audio)}
+        </button>
+    {/each}
+
+    <h2>Download stream probes</h2>
+    <button on:click={() => downloadFile(`${metadata.title}-video.json`, 'application/json', JSON.stringify(metadata.video.probe, null, 4))}>
+        <Icon icon="file-video" />
+        Video
+    </button>
+    {#each metadata.audio as audio, i}
+        <button on:click={() => downloadFile(`${metadata.title}-audio-${getTrackTitle(audio)}.json`, 'application/json', JSON.stringify(audio.probe, null, 4))}>
+            <Icon icon="file-audio" />
             {getTrackTitle(audio)}
         </button>
     {/each}
@@ -26,8 +42,8 @@
 
         {#each metadata.subtitles as subs}
             <button on:click={() => downloadFile(`${getTrackTitle(subs)}.${subs.format}`, 'text/plain', subs.content)}>
-                <Icon icon="download" />
-                {getTrackTitle(subs)}
+                <Icon icon="closed-captioning" />
+                {getTrackTitle(subs)} [{subs.format.toUpperCase()}]
             </button>
         {/each}
     {/if}
