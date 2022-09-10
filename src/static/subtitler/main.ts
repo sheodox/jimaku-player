95a1a5bf33a42e1e78fdb1dea360121d0592c143
sheodox <imagamer27@msn.com>
2022-09-10 12:12:54 -0500

    More tolerant initialization
    This makes it so if the container for the subtitler app isn't found it will keep retrying until it is found.
    
    This should enhance compatibility with extensions like Crunchyroll With Better Seasons which interrupt the page loading and the vilosRoot element isn't found immediatley.

--- src/static/subtitler/main.ts
+++ src/static/subtitler/main.ts
@@ -4,21 +4,38 @@ import { styles } from 'sheodox-ui';
 import './style/style.scss';
 
 const mount = document.createElement('div');
-if (location.host.includes('crunchyroll')) {
-	//crunchyroll fullscreens the player container, not the entire document,
-	//need to put the subtitler in there or it won't be visible when fullscreened
-	document.getElementById('vilosRoot').appendChild(mount);
-} else {
-	document.body.appendChild(mount);
-}
-mount.id = 'sheodox-jimaku-player';
-mount.style.position = 'fixed';
-mount.style.width = '100%';
-mount.style.pointerEvents = 'none';
-// crunchyroll's player might have some overlays that catch all clicks,
-// make any of jimaku player's buttons unclickable
-mount.style.zIndex = '999999999';
 
-new App({
-	target: mount,
-});
+function findContainer() {
+	if (location.host.includes('crunchyroll')) {
+		//crunchyroll fullscreens the player container, not the entire document,
+		//need to put the subtitler in there or it won't be visible when fullscreened
+		return document.getElementById('vilosRoot');
+	} else {
+		return document.body;
+	}
+}
+
+function init() {
+	const container = findContainer();
+
+	if (!container) {
+		// try again and hope the body has loaded, this can happen with extensions like Crunchyroll With Better Seasons
+		// which interrupt the page loading so the player container might not be there immediately
+		return setTimeout(init, 10);
+	}
+	container.appendChild(mount);
+
+	mount.id = 'sheodox-jimaku-player';
+	mount.style.position = 'fixed';
+	mount.style.width = '100%';
+	mount.style.pointerEvents = 'none';
+	// crunchyroll's player might have some overlays that catch all clicks,
+	// make any of jimaku player's buttons unclickable
+	mount.style.zIndex = '999999999';
+
+	new App({
+		target: mount,
+	});
+}
+
+init();
